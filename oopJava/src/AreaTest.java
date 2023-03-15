7ebb334d8b79d5da6092444bc26b91a6f42e9637
jjh75607 <dkrak5738@naver.com>
2023-03-15 14:42:59 +0900

    2주 실습 02

---
+++ oopJava/src/AreaTest.java
@@ -0,0 +1,9 @@
+public class AreaTest {
+    public static void main(String[] args) {
+        double pi = 3.141592;
+        double radius = 3.0;
+        double area = radius * radius * pi;
+
+        System.out.println("반지름이" + radius + "인 원의 면적은" + area + "입니다.");
+    }
+}
